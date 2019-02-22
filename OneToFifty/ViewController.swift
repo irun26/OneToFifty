1f584528619737111032611fdc2f8c779819b969
Jimson Vedua <jmsn133@gmail.com>
2019-02-22 00:09:07 -0500

    tableview done. cells 1 to 50.

--- OneToFifty/ViewController.swift
+++ OneToFifty/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return 55
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
